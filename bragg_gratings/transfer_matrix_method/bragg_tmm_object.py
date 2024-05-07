1050e9f194a5f3ddc04ef9387813e3cd2e76ff39
mustafacc <mustafastation@gmail.com>
2024-05-06 17:53:19 -0700

    bragg tmm object refactoring
    cleaned up the code. there are a bunch of unused variables, unused arguements, etc.
     also reimplemented it such that there is only a single bragg object for both 1310 and 1550. 1310 is now a subclass of 1550.

--- bragg_gratings/transfer_matrix_method/bragg_tmm_object.py
+++ bragg_gratings/transfer_matrix_method/bragg_tmm_object.py
@@ -1,23 +1,32 @@
 # -*- coding: utf-8 -*-
 """
 @author: Mustafa Hammood
+@adapted by: Bobby Zou to add apodization
 """
 # %%
 
+import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+import math
+import cmath
+import matplotlib.pyplot as plt
+import scipy.io
 
-class bragg_wg:
+
+class bragg_wg_1550:
     def __init__(
         self,
         width=0.5e-6,
         thickness=220e-9,
-        dw=20e-9,
-        period=318e-9,
+        dw=15e-9,
+        period=317e-9,
         N=300,
         alpha_dBcm=3,
         wavl_start=1520e-9,
         wavl_stop=1580e-9,
         resolution=0.1e-9,
-        segments=10,
+        gaussianIndex=2,
     ):
         self.width = width
         self.thickness = thickness
@@ -28,11 +37,9 @@ class bragg_wg:
         self.wavl_start = wavl_start
         self.wavl_stop = wavl_stop
         self.resolution = resolution
-        self.segments = segments  # simulation segments
+        self.gaussianIndex = gaussianIndex
 
-        self.poly, self.n1_reg, self.n2_reg, self.n3_reg = self.neff_lookup(
-            width, thickness
-        )
+        self.poly, self.n1_reg, self.n2_reg, self.n3_reg = self.neff_lookup()
         self.vis_shown = False
 
         self.neff0_values = [
@@ -47,7 +54,6 @@ class bragg_wg:
 
     @property
     def lambda_0(self):
-        import numpy as np
 
         return np.linspace(
             self.wavl_start,
@@ -62,6 +68,7 @@ class bragg_wg:
     @property
     def kappa(self):
         # TODO: load a lookup table instead...
+        # Waveguide dimension 500nm x 220nm
         return -1.53519e19 * self.dw**2 + 2.2751e12 * self.dw
 
     @property
@@ -75,8 +82,6 @@ class bragg_wg:
 
     @property
     def alpha(self):
-        import numpy as np
-
         return np.log(10) * self.alpha_dBcm / 10 * 100.0  # per meter
 
     def euclidean_distance(self, a, b):
@@ -85,23 +90,8 @@ class bragg_wg:
     def normalize(value, min_value, max_value):
         return (value - min_value) / (max_value - min_value)
 
-    def neff_lookup(self, width, thickness):
-        from sklearn.preprocessing import PolynomialFeatures
-        from sklearn.linear_model import LinearRegression
-        import numpy as np
-
+    def neff_lookup(self, file_path="wg_data/wg_variability_1550.txt"):
         # load waveguide neff data from lookup table
-        file_path = "wg_data/wg_variability.txt"
-        column_names = [
-            "thickness",
-            "width",
-            "wavl_start",
-            "wavl_stop",
-            "n1",
-            "n2",
-            "n3",
-        ]
-
         data = np.loadtxt(file_path, delimiter=",")
         points = data[:, :2]  # width and thickness
         n_values = data[:, 4:]
@@ -118,8 +108,6 @@ class bragg_wg:
         return poly, n1_reg, n2_reg, n3_reg
 
     def neff0(self, lambda0, width, thickness, visualize=True):
-        import numpy as np
-        import matplotlib.pyplot as plt
 
         point = np.array([[thickness, width]])
         point_poly = self.poly.transform(point)
@@ -163,17 +151,13 @@ class bragg_wg:
 
         return neff0_value
 
-    def n1(self, lambda0, width, thickness, index):
+    def n1_param(self, index):
         return self.neff0_values[index] - self.n_delta / 2
 
-    def n2(self, lambda0, width, thickness, index):
+    def n2_param(self, index):
         return self.neff0_values[index] + self.n_delta / 2
 
     def HomoWG_Matrix(self, wavl, neff, l):
-        import math
-        import cmath
-        import numpy as np
-
         j = cmath.sqrt(-1)
         beta = 2 * math.pi * neff / wavl - j * self.alpha / 2
         v = [np.exp(j * beta * l), np.exp(-j * beta * l)]
@@ -181,8 +165,6 @@ class bragg_wg:
         return T_hw
 
     def IndexStep_Matrix(self, neff1, neff2):
-        import numpy as np
-
         a = (neff1 + neff2) / (2 * np.sqrt(neff1 * neff2))
         b = (neff1 - neff2) / (2 * np.sqrt(neff1 * neff2))
 
@@ -190,11 +172,7 @@ class bragg_wg:
         return T_is
 
     def optimized_matrix_mult(self, T):
-        from functools import reduce
-        import numpy as np
-
         matrices = T[:]
-        result = []
 
         while len(matrices) > 1:
             temp = []
@@ -207,38 +185,49 @@ class bragg_wg:
         return matrices[0]
 
     def Grating_Matrix(self, wavl, l, index):
-        import numpy as np
-        import random
-
-        # segment the grating length into self.segments
-        N_seg = int(self.N / self.segments)
 
         T = []
         i = 0
-        while i < self.segments:
-            # random variation of width
-            width_err = self.width + random.uniform(-115e-9, 115e-9)
-            n1_err = self.n1(wavl, width_err, self.thickness, index)
-            n2_err = self.n2(wavl, width_err, self.thickness, index)
-            l_err = l + 0
+        while i < self.N:
+            # apodization paramater addon
+            profileFunction = math.exp(
+                -0.5 * (2 * self.gaussianIndex * (i - self.N / 2) / (self.N)) ** 2
+            )
 
-            T_hw1 = self.HomoWG_Matrix(wavl, n1_err, l_err)
-            T_is12 = self.IndexStep_Matrix(n1_err, n2_err)
-            T_hw2 = self.HomoWG_Matrix(wavl, n2_err, l_err)
-            T_is21 = self.IndexStep_Matrix(n2_err, n1_err)
+            # From KLayout PCell "amf_bragg_apodized.py"
+            # "profile = int(round(self.corrugation_width/2/dbu))*profileFunction"
+            # Breaking down into setps from the above equation, I had to mimic the implmentation in KLayout but it is not necessary
+            profile = self.dw / 2 / 1e-9
+            profile = int(round(profile))
+            profile = profile * profileFunction
+
+            # Get total dwidth
+            # Option 1 - No apodization
+            # dwidth_apodized = self.dw
+
+            # Option 2 - With apodization
+            dwidth_apodized = profile * 2 * 1e-9
+
+            n1_final = self.n1_param(index)
+            n2_final = self.n2_param(index)
+            l_final = l + 0
+
+            T_hw1 = self.HomoWG_Matrix(wavl, n1_final, l_final)
+            T_is12 = self.IndexStep_Matrix(n1_final, n2_final)
+            T_hw2 = self.HomoWG_Matrix(wavl, n2_final, l_final)
+            T_is21 = self.IndexStep_Matrix(n2_final, n1_final)
 
             Tp1 = np.matmul(T_hw1, T_is12)
             Tp2 = np.matmul(T_hw2, T_is21)
             Tp = np.matmul(Tp1, Tp2)
-            T.append(np.linalg.matrix_power(Tp, N_seg))
+            T.append(Tp)
             i += 1
-        if self.segments == 1:
+        if self.N == 1:
             return T[0]
         else:
             return self.optimized_matrix_mult(T)
 
     def Grating_RT(self, wavl, index):
-        import numpy as np
 
         M = self.Grating_Matrix(wavl, self.l, index)
         T = np.absolute(1 / M[0][0]) ** 2
@@ -252,9 +241,11 @@ class bragg_wg:
             *[self.Grating_RT(wavl, index) for index, wavl in enumerate(self.lambda_0)]
         )
 
+        data_save = {"R": self.R, "T": self.T, "lambda_0": self.lambda_0}
+        # Save to a .mat file
+        scipy.io.savemat("braggResponse.mat", data_save)
+
     def visualize(self):
-        import matplotlib.pyplot as plt
-        import numpy as np
 
         if "self.R" not in globals() or "self.T" not in globals():
             self.Run()
@@ -275,10 +266,53 @@ class bragg_wg:
         ax.set_title("Calculated response of the structure using TMM (dB scale)")
 
 
-if __name__ == "__main__":
-    bragg = bragg_wg(
-        period=317e-9, dw=15e-9, N=300, width=500e-9, thickness=220e-9, segments=5
-    )
-    bragg.visualize()
+class bragg_wg_1310(bragg_wg_1550):
+    def __init__(
+        self,
+        width=0.35e-6,
+        thickness=220e-9,
+        dw=15e-9,
+        period=270e-9,
+        N=300,
+        alpha_dBcm=3,
+        wavl_start=1260e-9,
+        wavl_stop=1360e-9,
+        resolution=0.1e-9,
+        gaussianIndex=2,
+    ):
+        super().__init__(
+            width=width,
+            thickness=thickness,
+            dw=dw,
+            period=period,
+            N=N,
+            alpha_dBcm=alpha_dBcm,
+            wavl_start=wavl_start,
+            wavl_stop=wavl_stop,
+            resolution=resolution,
+            gaussianIndex=gaussianIndex,
+        )
 
+    @property
+    def kappa(self):
+        return -6.37130e19 * self.dw**2 + 8.61915e12 * self.dw + 6.87012e3
+
+    @property
+    def lambda_bragg(self):
+        return 1305.72e-9
+
+    def neff_lookup(self, file_path="wg_data/wg_variability_1310.txt"):
+        return super().neff_lookup(file_path=file_path)
+
+
+if __name__ == "__main__":
+    bragg_1550 = bragg_wg_1550(
+        period=317e-9, dw=15e-9, N=300, width=500e-9, thickness=220e-9, gaussianIndex=2
+    )
+    bragg_1550.visualize()
+
+    bragg_1310 = bragg_wg_1310(
+        period=270e-9, dw=15e-9, N=300, width=350e-9, thickness=220e-9, gaussianIndex=2
+    )
+    bragg_1310.visualize()
 # %%
