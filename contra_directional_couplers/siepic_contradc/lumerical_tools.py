250d86e4845018aaad6c2376545ca3a47a2c7563
Lukas Chrostowski <lukasc@ece.ubc.ca>
2025-03-28 16:48:26 -0700

    new: get_DesignKit(name) returns path

--- contra_directional_couplers/siepic_contradc/lumerical_tools.py
+++ contra_directional_couplers/siepic_contradc/lumerical_tools.py
@@ -8,40 +8,42 @@ import sys, os, platform
 mode = None # variable for the Lumerical Python API
 
 # Lumerical Python API path on system
+try:
+    import lumapi
+except:
+    cwd = os.getcwd()
 
-cwd = os.getcwd()
-
-if platform.system() == 'Darwin':
-    path_app = '/Applications'
-elif platform.system() == 'Linux':
-    path_app = '/opt'
-    os.environ["QT_QPA_PLATFORM"] = "xcb" 
-elif platform.system() == 'Windows': 
-    path_app = 'C:\\Program Files'
-    os.environ['QT_QPA_PLATFORM']= 'windows'
-    print('Windows Qt')
-else:
-    print('Not a supported OS')
-    exit()
+    if platform.system() == 'Darwin':
+        path_app = '/Applications'
+    elif platform.system() == 'Linux':
+        path_app = '/opt'
+        os.environ["QT_QPA_PLATFORM"] = "xcb" 
+    elif platform.system() == 'Windows': 
+        path_app = 'C:\\Program Files'
+        os.environ['QT_QPA_PLATFORM']= 'windows'
+        print('Windows Qt')
+    else:
+        print('Not a supported OS')
+        exit()
 
 
-# Application folder paths containing Lumerical
-p = [s for s in os.listdir(path_app) if "lumerical" in s.lower()]
-# check sub-folders for lumapi.py
-import fnmatch
-for dir_path in p:
-    search_str = 'lumapi.py'
-    matches = []
-    for root, dirnames, filenames in os.walk(os.path.join(path_app,dir_path), followlinks=True):
-        for filename in fnmatch.filter(filenames, search_str):
-            matches.append(root)
-    if matches:
-        lumapi_path = matches[0]
-if not lumapi_path in sys.path:
-    sys.path.append(lumapi_path)
-#    os.chdir(lumapi_path)
+    # Application folder paths containing Lumerical
+    p = [s for s in os.listdir(path_app) if "lumerical" in s.lower()]
+    # check sub-folders for lumapi.py
+    import fnmatch
+    for dir_path in p:
+        search_str = 'lumapi.py'
+        matches = []
+        for root, dirnames, filenames in os.walk(os.path.join(path_app,dir_path), followlinks=True):
+            for filename in fnmatch.filter(filenames, search_str):
+                matches.append(root)
+        if matches:
+            lumapi_path = matches[0]
+    if not lumapi_path in sys.path:
+        sys.path.append(lumapi_path)
+    #    os.chdir(lumapi_path)
 
-print('Lumerical lumapi.py path: %s' % lumapi_path)
+    print('Lumerical lumapi.py path: %s' % lumapi_path)
 
 import lumapi
 
@@ -96,3 +98,32 @@ def run_INTC():
     lumapi.evalScript(intc, command)
     
     return
+
+
+def get_DesignKit(name):
+    '''
+    Query Lumerical INTERCONNECT to find the path for the specific design kit
+    '''
+    intc = lumapi.open('interconnect')
+
+    # Get all the library elements
+    command = 'elements=library;'
+    lumapi.evalScript(intc, command)
+    intc_elements = lumapi.getVar(intc, "elements")
+    intc_elements = intc_elements.split('\n')
+    # find the ones that match the requested Design Kit name
+    j=[i for i in intc_elements if '::design kits::'+name.lower() in i]
+    if not j:
+        raise Exception('No elements in the Design Kit "%s" found.' % name)
+    # find the first element in the root folder
+    intc_element=[i for i in j if len(i.split('::'))==4][0]
+    # get the path
+    command = f'addelement("{intc_element}"); \n'
+    command += 'path=get("local path");'
+    lumapi.evalScript(intc, command)
+    intc_designkit_path = lumapi.getVar(intc, "path")
+
+    return intc_designkit_path
+
+if __name__ == "__main__":
+    print ( get_DesignKit('EBeam') )
